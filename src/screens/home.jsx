1ecb5c7d1088e8f3556488415c0e903c8ee16930
Deepak Dev <deepakplay14@gmail.com>
2023-09-10 11:24:50 +0530

    Images add

--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -14,7 +14,7 @@ const Home = ({ navigation }) => {
             <View className="py-2 px-4 flex-row gap-x-2 items-center">
                 <View>
                     <Image
-                        source={{ uri: 'https://links.papareact.com/wru' }}
+                        source={require('../images/image0.jpg')}
                         className="bg-gray-200 p-5 rounded-full"
                     />
                 </View>
