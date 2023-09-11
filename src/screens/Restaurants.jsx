9d3fa37a14d9a5caf24ffe9aae50a04f3a83e2f6
Deepak Dev <deepakplay14@gmail.com>
2023-09-11 07:47:10 +0530

    Restarunt page #1

--- src/screens/Restaurants.jsx
+++ src/screens/Restaurants.jsx
@@ -1,15 +1,23 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView, Image, ImageBackground, TouchableOpacity } from 'react-native'
 import React from 'react'
 import SafeAreaView from '../components/SafeAreaView/SafeAreaView'
 import { useRoute } from '@react-navigation/core'
+import { ArrowLeftIcon } from 'react-native-heroicons/solid';
+import { useNavigation } from '@react-navigation/core';
 
 const Restaurants = () => {
-    const { params: { title } } = useRoute();
+    const navigate = useNavigation();
+    // const { params: { title } } = useRoute();
+
     return (
-        <ScrollView>
-            <SafeAreaView className="flex-1 bg-slate-800">
-                <Text className="text-white">Test</Text>
-            </SafeAreaView>
+        <ScrollView className="flex-1">
+            <View className="w-full h-52">
+                <ImageBackground source={require('../images/image2.jpg')} className="w-full h-full relative">
+                    <TouchableOpacity className="w-10 h-10 bg-neutral-700 rounded-full absolute top-2 left-2 justify-center items-center" onPress={navigate.goBack}>
+                        <ArrowLeftIcon size={20} color="#00CCBB" />
+                    </TouchableOpacity>
+                </ImageBackground>
+            </View>
         </ScrollView>
     )
 }
